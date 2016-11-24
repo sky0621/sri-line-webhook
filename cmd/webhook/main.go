134f42ac90aaee2b225899240f9f0a21fb786975
sky0621 <sky14work@gmail.com>
2016-11-25 01:20:07 +0900

    fix

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -18,6 +21,8 @@ var bot *linebot.Client
 
 var applog *logger
 
+var chaSec string
+
 func main() {
 	s := flag.String("s", "ChannelSecret", "ChannelSecret")
 	t := flag.String("t", "AccessToken", "AccessToken")
@@ -30,9 +35,14 @@ func main() {
 	defer logfile.Close()
 
 	applog = &logger{isDebugEnable: true}
+	applog.debug("&&&&&&&&&&&&&&&&&&&&&&&&&")
 	applog.debug("START")
+	applog.debug(*s)
+	applog.debug(*t)
+	applog.debug("&&&&&&&&&&&&&&&&&&&&&&&&&")
 
-	bot, err = linebot.New(*s, *t)
+	chaSec = *s
+	bot, err = linebot.New(chaSec, *t)
 	if err != nil {
 		applog.errorf("[main]", "Err: %+v", err)
 		return
@@ -45,6 +55,93 @@ func main() {
 }
 
 func srrHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := ParseRequest(chaSec, r)
+	if err != nil {
+		applog.errorf("[srrHandler]", "Err: %+v", err)
+	}
+
+	applog.debug("[srrHandler]", "Event for start")
+
+	for _, event := range events {
+		applog.debugf("[srrHandler]", "Event Type: %+v", event.Type)
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				applog.debugf("[srrHandler]", "Event Message Type: %+v", message)
+				var newMsg *linebot.TextMessage
+				if "あぶない" == message.Text {
+					newMsg = linebot.NewTextMessage("ばしょをちずでおしえて！")
+				} else {
+					newMsg = linebot.NewTextMessage(message.Text + "!?")
+				}
+				applog.debugf("[srrHandler]", "newMsg: %s", newMsg)
+
+				repMsg := bot.ReplyMessage(event.ReplyToken, newMsg)
+				applog.debugf("[srrHandler]", "replyMessage: %+v", repMsg)
+				if _, err = bot.ReplyMessage(event.ReplyToken, newMsg).Do(); err != nil {
+					applog.errorf("[srrHandler]", "Err: %+v", err)
+				}
+			case *linebot.LocationMessage:
+				applog.debugf("[srrHandler]", "Event Message Type: %+v", message)
+				lat := message.Latitude
+				lon := message.Longitude
+				addr := message.Address
+				retMsg := fmt.Sprintf("じゅうしょは、%s \n緯度：%f\n経度：%f\nだね。ありがとう。みんなにもおしえてあげるね。", addr, lat, lon)
+				applog.debugf("[srrHandler]", "retMsg: %s", retMsg)
+				newMsg := linebot.NewTextMessage(retMsg)
+				repMsg := bot.ReplyMessage(event.ReplyToken, newMsg)
+				applog.errorf("[srrHandler]", "replyMessage: %+v", repMsg)
+				if _, err = bot.ReplyMessage(event.ReplyToken, newMsg).Do(); err != nil {
+					applog.errorf("[srrHandler]", "Err: %+v", err)
+				}
+			}
+		}
+	}
+
+}
+
+// ParseRequest func
+func ParseRequest(channelSecret string, r *http.Request) ([]*linebot.Event, error) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	applog.debugf("[ParseRequest]", "channelSecret: %s", channelSecret)
+
+	lineSign := r.Header.Get("X-Line-Signature")
+	applog.debugf("[ParseRequest]", "X-Line-Signature: %s", lineSign)
+	if !validateSignature(channelSecret, lineSign, body) {
+		return nil, linebot.ErrInvalidSignature
+	}
+	applog.debug("[ParseRequest]", "valiate ok")
+
+	request := &struct {
+		Events []*linebot.Event `json:"events"`
+	}{}
+	if err = json.Unmarshal(body, request); err != nil {
+		return nil, err
+	}
+	applog.debugf("[ParseRequest]", "==##==##==##==##==##==##==##==##==")
+	applog.debugf("[ParseRequest]", "request: %+v", request)
+	applog.debugf("[ParseRequest]", "==##==##==##==##==##==##==##==##==")
+
+	return request.Events, nil
+}
+
+func validateSignature(channelSecret, signature string, body []byte) bool {
+	decoded, err := base64.StdEncoding.DecodeString(signature)
+	applog.debugf("[validateSignature]", "decoded: %s", decoded)
+	if err != nil {
+		return false
+	}
+	hash := hmac.New(sha256.New, []byte(channelSecret))
+	hash.Write(body)
+	applog.debugf("[validateSignature]", "hash.Sum(nil): %s", hash.Sum(nil))
+	return hmac.Equal(decoded, hash.Sum(nil))
+}
+
+func srrHandler2(w http.ResponseWriter, r *http.Request) {
 	ba, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		applog.errorf("[srrHandler]", "Err: %+v", err)
@@ -55,24 +152,31 @@ func srrHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		applog.debugf("[srrHandler]", "RequestBody: %+v", string(ba))
 	}
-	// events, err := bot.ParseRequest(r)
-	defer r.Body.Close()
+
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		applog.errorf("[srrHandler]", "Err: %+v", err)
 	}
-	applog.errorf("[srrHandler]", "channelSecret: x%+vs", r.Header.Get("X-Line-Signature"))
+	applog.debugf("[srrHandler]", "$$$ RequestBody $$$ : %+v", body)
+
+	events, err := bot.ParseRequest(r)
+	// defer r.Body.Close()
+	// body, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	applog.errorf("[srrHandler]", "Err: %+v", err)
+	// }
+	// applog.errorf("[srrHandler]", "channelSecret: x%+vs", r.Header.Get("X-Line-Signature"))
 	// if !validateSignature(channelSecret, r.Header.Get("X-Line-Signature"), body) {
 	// 	return nil, ErrInvalidSignature
 	// }
 
-	request := &struct {
-		Events []*linebot.Event `json:"events"`
-	}{}
-	if err = json.Unmarshal(body, request); err != nil {
-		applog.errorf("[srrHandler]", "Err: %+v", err)
-	}
-	events, err := request.Events, nil
+	// request := &struct {
+	// 	Events []*linebot.Event `json:"events"`
+	// }{}
+	// if err = json.Unmarshal(body, request); err != nil {
+	// 	applog.errorf("[srrHandler]", "Err: %+v", err)
+	// }
+	// events, err := request.Events, nil
 
 	if err != nil {
 		applog.errorf("[srrHandler]", "Err: %+v", err)
